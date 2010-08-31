cbf049d3d0df06a83edf36b59c2fc7d2c68407a6
Brian Kaney <brian@vermonster.com>
2010-08-30 22:40:24 -0400

    Updating compass to use Rails.root

--- config/compass.rb
+++ config/compass.rb
@@ -3,7 +3,7 @@ require 'ninesixty'
 # Require any additional compass plugins here.
 
 project_type = :rails
-project_path = RAILS_ROOT if defined?(RAILS_ROOT)
+project_path = Rails.root # RAILS_ROOT if defined?(RAILS_ROOT)
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "public/stylesheets/compiled"
--- config/initializers/compass.rb
+++ config/initializers/compass.rb
@@ -1,5 +1,5 @@
 require 'compass'
-rails_root = (defined?(Rails) ? Rails.root : RAILS_ROOT).to_s
+rails_root = Rails.root # (defined?(Rails) ? Rails.root : RAILS_ROOT).to_s
 Compass.add_project_configuration(File.join(rails_root, "config", "compass.rb"))
 Compass.configure_sass_plugin!
 Compass.handle_configuration_change!
