cc8fc9674def7b0dec3366e8f58edec831701108
Brian Kaney <brian@vermonster.com>
2010-08-31 00:21:53 -0400

    Fixed a bug in inherited_resources (actually already there, but the last IR release was a few months ago).

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   layout 'application'
+
+  protected 
+
+  def collection
+    get_collection_ivar || set_collection_ivar(end_of_association_chain.all)
+  end
+
 end
